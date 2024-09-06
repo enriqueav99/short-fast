b0d15d93d30ae0812e1acffd211fa813f35eb51c
enriqueav99 <enriqueav1999@gmail.com>
2024-09-06 21:36:04 +0000

    migrate to flask
    Signed-off-by: enriqueav99 <enriqueav1999@gmail.com>

--- reciver/main.py
+++ reciver/main.py
@@ -1,25 +1,32 @@
-from fastapi import FastAPI
-import uvicorn
+from flask import Flask, jsonify
+import os
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/")
+@app.route("/")
 def read_root():
     return "Hola mundo!"
 
-@app.get("/carpeta")
-def read_root():
+@app.route("/carpeta")
+def list_mp4_files():
     archivos_mp4 = []
-    for archivo in os.listdir(doomy):
+    directorio = "doomy"  # Reemplaza "doomy" con la ruta real del directorio
+    for archivo in os.listdir(directorio):
         if archivo.endswith('.mp4'):
             archivos_mp4.append(archivo)
 
     if archivos_mp4:
-        print("Archivos .mp4 encontrados:")
-        for nombre in archivos_mp4:
-            print(nombre)
+        response = {
+            "message": "Archivos .mp4 encontrados:",
+            "archivos": archivos_mp4
+        }
     else:
-        print("No se encontraron archivos .mp4 en la carpeta.")
+        response = {
+            "message": "No se encontraron archivos .mp4 en la carpeta."
+        }
+
+    return jsonify(response)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    app.run(host="127.0.0.1", port=8000, debug=True)
+
